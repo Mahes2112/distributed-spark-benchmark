c507b527b04fbf4db17154e3347e8a2a7d10cf41
Maheshwaan B <mahes.rjs@gmail.com>
2026-02-24 18:44:57 +0530

    Create test_spark_session.py

---
+++ tests/test_spark_session.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+def test_spark_session():
+    spark = SparkSession.builder \
+        .master("local[1]") \
+        .appName("ci-test") \
+        .getOrCreate()
+
+    data = [("A", 1), ("B", 2)]
+    df = spark.createDataFrame(data, ["name", "value"])
+
+    assert df.count() == 2
+
+    spark.stop()
