5b5a618d86d4007996d00223c088d5dbc4094adb
Mahes2112 <mahes.rjs@gmail.com>
2026-02-24 19:14:43 +0530

    Apply black formatting

--- tests/test_spark_session.py
+++ tests/test_spark_session.py
@@ -1,10 +1,8 @@
 from pyspark.sql import SparkSession
 
+
 def test_spark_session():
-    spark = SparkSession.builder \
-        .master("local[1]") \
-        .appName("ci-test") \
-        .getOrCreate()
+    spark = SparkSession.builder.master("local[1]").appName("ci-test").getOrCreate()
 
     data = [("A", 1), ("B", 2)]
     df = spark.createDataFrame(data, ["name", "value"])
